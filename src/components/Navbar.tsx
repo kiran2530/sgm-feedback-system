a5253b2e71f210f1d27af98c6ef6997f124309b4
kirannikam <nikamsarkar464@gmail.com>
2025-06-08 20:04:57 +0530

    access changed questions in create feedback model.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,75 +10,8 @@ import { Feedback } from "@/types";
 import { createFeedbackFormAction } from "@/actions/feedbacks";
 import LoginModal from "./LoginModal";
 import AdminRegistrationModal from "./AdminRegistrationModal";
+import { feedbackQuestions } from "@/data/feedbackQuestionsOption";
 // import { checkLogin } from "@/utils/checkLogin";
-
-const feedbackQuestions = [
-  {
-    id: 1,
-    question:
-      "How would you rate the overall teaching effectiveness of the faculty?",
-  },
-  { id: 2, question: "How well does the faculty explain complex topics?" },
-  { id: 3, question: "Does the faculty encourage student participation?" },
-  {
-    id: 4,
-    question: "How clear are the faculty’s explanations during lectures?",
-  },
-  { id: 5, question: "How responsive is the faculty to students' queries?" },
-  { id: 6, question: "How well does the faculty manage class time?" },
-  {
-    id: 7,
-    question: "Does the faculty provide relevant examples while teaching?",
-  },
-  {
-    id: 8,
-    question: "How effective are the faculty's assessment and grading methods?",
-  },
-  {
-    id: 9,
-    question:
-      "Does the faculty provide sufficient study materials and references?",
-  },
-  {
-    id: 10,
-    question: "How well does the faculty encourage critical thinking?",
-  },
-  {
-    id: 11,
-    question: "How approachable is the faculty outside of class hours?",
-  },
-  {
-    id: 12,
-    question: "Does the faculty use modern teaching methods effectively?",
-  },
-  {
-    id: 13,
-    question: "How satisfied are you with the faculty's subject knowledge?",
-  },
-  {
-    id: 14,
-    question: "Does the faculty provide constructive feedback on assignments?",
-  },
-  {
-    id: 15,
-    question:
-      "How well does the faculty connect theoretical concepts to practical applications?",
-  },
-  {
-    id: 16,
-    question: "Does the faculty create an engaging learning environment?",
-  },
-  { id: 17, question: "How effective are the faculty’s communication skills?" },
-  {
-    id: 18,
-    question: "How satisfied are you with the faculty's classroom management?",
-  },
-  {
-    id: 19,
-    question: "Does the faculty encourage teamwork and collaboration?",
-  },
-  { id: 20, question: "Would you recommend this faculty to other students?" },
-];
 
 // const ratingOptions: number[] = [
 //   0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5,
